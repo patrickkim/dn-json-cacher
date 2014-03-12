44f676839812bca1b54e050df82c00c47c2d26cc
Patrick Kim <patrickskim@me.com>
2014-03-11 23:11:49 -0400

    Time.parse string_time to do Time evals

--- mem_fetcher.rb
+++ mem_fetcher.rb
@@ -1,23 +1,25 @@
+require "rubygems"
+require "json"
+require "redis"
+
 class MemFetcher
 
   def fetch(url, max_age = 0)
     #response sets it to nil if key doesnt exist
-    @cache = REDIS.get(url)
+    @redis_cache = REDIS.get(url)
 
-    if @cache
-      cache_response = JSON.parse @cache
-      return cache_response["response"] if Time.now - cache_response["time_stamp"] < max_age
+    if @redis_cache
+      cached_response = JSON.parse @redis_cache
+      time_diff = Time.now - Time.parse cached_response["time_stamp"]
+      return cached_response["response"] if time_diff < max_age
     end
 
     cache = {
-      "response" => "{testing: response_test}",
-      # Net::HTTP.get_response(URI.parse(url)).body,
+      "response" => Net::HTTP.get_response(URI.parse(url)).body,
       "time_stamp" => Time.now
     }
 
     REDIS.set url, cache.to_json
-
-    binding.pry
     return cache["response"]
   end
 
