76cea2960fc82841b0e8d013b10e5067ca04c208
Patrick Kim <patrickskim@me.com>
2014-03-12 00:48:02 -0400

    working verison super forefully

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
-require "rubygems"
-require "sinatra"
+require "net/http"
+require "base64"
 require "json"
 require "redis"
-require "./mem_fetcher"
+require "sinatra"
 
 configure do
   redis_uri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
   uri = URI.parse(redis_uri)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  MAX_AGE = 300
 end
 
 get "/" do
@@ -16,17 +17,49 @@ get "/" do
 end
 
 get "/r" do
-  cache ||= MemFetcher.new
-  api_response = cache.fetch("https://news.layervault.com/?format=json", 300)
+  url = "https://news.layervault.com/?format=json"
+
+  @redis_cache = REDIS.get(url)
+  if @redis_cache
+    cached_response = JSON.parse @redis_cache
+    time_diff = Time.now - Time.parse(cached_response["time_stamp"])
+    @api_response = Base64.decode64(cached_response["response"])
+    REDIS.del(url) if time_diff < MAX_AGE
+  else
+    raw_response = Net::HTTP.get_response(URI.parse(url)).body
+    cache = {
+      "response" => Base64.encode64(raw_response),
+      "time_stamp" => Time.now
+    }
+
+    REDIS.set url, cache.to_json
+    @api_response = raw_response
+  end
 
   content_type:json
-  api_response
+  @api_response
 end
 
 get "/n" do
-  cache ||= MemFetcher.new
-  api_response = cache.fetch("https://news.layervault.com/new?format=json", 300)
+  url= "https://news.layervault.com/new?format=json"
+
+  @redis_cache = REDIS.get(url)
+  if @redis_cache
+    cached_response = JSON.parse @redis_cache
+    time_diff = Time.now - Time.parse(cached_response["time_stamp"])
+    @api_response = Base64.decode64(cached_response["response"])
+    REDIS.del(url) if time_diff < MAX_AGE
+  else
+    raw_response = Net::HTTP.get_response(URI.parse(url)).body
+    cache = {
+      "response" => Base64.encode64(raw_response),
+      "time_stamp" => Time.now
+    }
+
+    REDIS.set url, cache.to_json
+    @api_response = raw_response
+  end
 
   content_type:json
-  api_response
+  @api_response
 end
